312d304f4a389d332c1fee86743c3166a4b91542
Successtsk <138924773+Successtsk@users.noreply.github.com>
2023-07-07 20:11:49 +0000

    added functions to calculate arrays

--- CGPA.js
+++ CGPA.js
@@ -4,6 +4,8 @@ let unitEl = document.querySelectorAll("unit-el")
 let scoreEl = document.querySelectorAll(".score-el")
 let gpaEl = document.getElementById("gpa-el")
 let gpaArray = []
+let gradeArray = []
+let unitArray = []
 let grade 
 let A = 5
 let B = 4
@@ -13,39 +15,60 @@ let F = 0
 
 
 
- firstBtn.addEventListener("click", function(){
- calculategpa()
- })
-
     function calculategpa(){
-      
      scoreEl.forEach(function(node){
-        gpaArray.push(+node.value)
+        let score = +node.value
+        if (score <= 44){
+            grade = F
+        } else if (score < 50 ){
+            grade = D
+        } else if(score < 60){
+            grade = C
+        } else if(score < 70){
+            grade = B
+        } else if(score < 101){
+            grade = A
+        } 
+        gradeArray.push(grade)
      }
      )
-     console.log(gpaArray)
-
      
-      
-     console.log(scoreEl.value)     
-        if (scoreEl.value <= 44){
-           grade = F
-       } else if (scoreEl.value < 50 ){
-           grade = D
-       } else if(scoreEl.value < 60){
-           grade = C
-       } else if(scoreEl.value < 70){
-           grade = B
-       } else if(scoreEl.value < 101){
-           grade = A
-       } 
-      
-    
-    
-
-    
-
-
-    /* let gpa = grade * unitEl.value
-    gpaEl.textContent = gpa */
+  unitEl.forEach(function(node){
+    let unit = +node.value
+    unitArray.push(unit)
+  }
+  )    
+//   as new values are inputed its being added to the total
+let totalGrade = 0
+for (let i = 0; i < gradeArray.length; i++) {
+  totalGrade += gradeArray[i];
 }
+
+let totalUnits = 0
+for (let i = 0; i <unitArray.length; i++) {
+  totalUnits += unitArray[i]; 
+}
+
+let gpa = totalGrade/totalUnits
+return gpa 
+}
+
+
+
+
+
+firstBtn.addEventListener("click", function(){
+  gpaEl.innerText = calculategpa()
+  })
+
+
+//   function sum(array) {
+//     let totalSum = 0
+//     for (let index = 0; index < array.length; index++) {
+//         totalSum += array[index];
+//     }
+//     return totalSum
+//   }
+
+//   totalGrade = sum(gradeArray)
+//   totalUnits = sum(unitArray)
